501172829ec758824b623e6ae8be0cd5358d243d
Bri@n <bdonaldson@pm.me>
2020-12-05 19:12:17 -0500

    Added Test Case

---
+++ src/main/java/com/tenderretail/utils/PhoneNumberValidator.java
@@ -0,0 +1,32 @@
+/*
+ * Contactless Cons WebApp by Brian Donaldson
+ * Copyright (c) 2020.  Brian Donaldson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.tenderretail.utils;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+
+@Service
+public class PhoneNumberValidator implements Predicate<String> {
+
+    @Override
+    public boolean test(String phoneNumber) {
+        return phoneNumber.startsWith("+44") && phoneNumber.length() == 13;
+    }
+}
---
+++ src/test/java/com/tenderretail/utils/PhoneNumberValidatorTest.java
@@ -0,0 +1,49 @@
+/*
+ * Contactless Cons WebApp by Brian Donaldson
+ * Copyright (c) 2020.  Brian Donaldson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.tenderretail.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PhoneNumberValidatorTest {
+
+    private PhoneNumberValidator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new PhoneNumberValidator();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "+447000000000,true",
+            "+44700000000088878, false",
+            "447000000000, false"
+    })
+    void itShouldValidatePhoneNumber(String phoneNumber, boolean expected) {
+        // When
+        boolean isValid = underTest.test(phoneNumber);
+
+        // Then
+        assertThat(isValid).isEqualTo(expected);
+    }
+}
